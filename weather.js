f7a54d844df38ed41a2c391faf075ac8bbb807f6
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-10-14 00:32:47 +0900

    add API
    add API

--- weather.js
+++ weather.js
@@ -1,3 +1,38 @@
+const API_KEY = "81497240f61a2131345c48fdfa585cb6";
+
+const COORDS = "coords";
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError(){
+    console.log("Cant access geo location");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces,handleGeoError)
+}
+
+function loadCoords(){
+    const loadedCords = localStorage.getItem(COORDS);
+    if(loadedCords === null) {
+        askForCoords();
+    }else{
+        //get Weather
+    }
+}
+
 function init(){
 
 }
